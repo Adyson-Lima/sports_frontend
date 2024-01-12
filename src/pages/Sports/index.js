39a7e8c724ddbff2e16501efc73bb2ddd1e92c35
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-12 12:35:58 -0300

    9° commit - tdd tela Sports: Existe botão editar em Sports? GREEN

--- src/pages/Sports/index.js
+++ src/pages/Sports/index.js
@@ -31,6 +31,9 @@ export default function Sports(){
                 <td></td>
                 <td>
 
+                  <button data-testid="mybtn1" type="button"
+                  className="btn btn-outline-info">Editar</button>
+
                 </td>
             </tr>
           </tbody>
