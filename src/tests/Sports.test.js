4b86940c6ae8a603dcae75641ebb358a302dcab6
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-12 12:37:50 -0300

    10° commit - tdd tela Sports: Existe botão excluir em Sports? RED

--- src/tests/Sports.test.js
+++ src/tests/Sports.test.js
@@ -28,4 +28,8 @@ describe('Testes da tela Sports', () => {
     expect(screen.getByTestId('mybtn1')).toBeInTheDocument();
   });
 
+  it('Existe botão excluir em Sports?', () => {
+    expect(screen.getByTestId('mybtn2')).toBeInTheDocument();
+  });
+
 });
