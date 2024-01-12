d18ba4ddd828402c1cc563df3bad9cd90eed2be7
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-12 12:24:16 -0300

    3° commit - tdd tela Sports: Existe card em Sports? GREEN

---
+++ src/pages/Sports/index.js
@@ -0,0 +1,20 @@
+import { Link, useNavigate, useParams } from "react-router-dom";
+import React,{useState, useEffect} from "react";
+// import api from '../../services/api';
+
+export default function Sports(){
+
+  return(
+
+    <div data-testid="mycard" className="card border-primary" style={{marginTop: '20px'}} >
+      <div className="card-header bg-primary" style={{color: '#fff'}}>
+        Sports Crud
+      </div>
+      <div className="card-body">
+
+      </div>
+    </div>
+
+  );
+
+}
